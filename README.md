8e47ae6e0963c59dfa0e61acfe367403ec7495ca
jpalucki <jan.palucki@miquido.com>
2022-06-13 12:49:17 +0200

    feat: Swagger docs for service endpoints

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pizza-service
-Pizza Service as an Interview task
+Pizza Service as an Interview task.
+
+Service exposes 4 simple endpoints of basic CRUD operations.
+
+After starting application locally please head up to this
+[link](http://localhost:8080/api/v1/swagger-ui/index.html?configUrl=/api/v1/v3/api-docs/swagger-config#).
--- src/main/java/com/jpalucki/pizzaservice/controller/PizzaController.java
+++ src/main/java/com/jpalucki/pizzaservice/controller/PizzaController.java
@@ -2,6 +2,9 @@ package com.jpalucki.pizzaservice.controller;
 
 import com.jpalucki.pizzaservice.service.*;
 import com.jpalucki.pizzaservice.service.dto.*;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.responses.*;
+import io.swagger.v3.oas.annotations.tags.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.*;
@@ -9,7 +12,9 @@ import org.springframework.web.servlet.support.*;
 import javax.servlet.http.*;
 import java.util.*;
 
-@RestController("pizzas")
+@Tag(name = "PizzaService API - Pizzas Controller")
+@RestController
+@RequestMapping("pizzas")
 public class PizzaController {
 
     private final PizzaService pizzaService;
@@ -18,11 +23,21 @@ public class PizzaController {
         this.pizzaService = pizzaService;
     }
 
+    @Operation(summary = "Get all pizzas.")
+    @ApiResponses(
+        @ApiResponse(responseCode = "200", description = "OK.")
+    )
     @GetMapping
     public Collection<PizzaDTO> getPizzas() {
         return pizzaService.getPizzas();
     }
 
+    @Operation(summary = "Create new pizza.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK."),
+        @ApiResponse(responseCode = "400", description = "Bad Request."),
+        @ApiResponse(responseCode = "422", description = "Unprocessable Entity.")
+    })
     @PostMapping
     public PizzaDTO createPizza(@RequestBody PizzaDTO pizzaDTO, HttpServletResponse response) {
         PizzaDTO createdPizza = pizzaService.createPizza(pizzaDTO);
@@ -36,12 +51,24 @@ public class PizzaController {
         return createdPizza;
     }
 
-    @PutMapping
-    public void updatePizza(@RequestBody PizzaDTO pizzaDTO, HttpServletResponse response) {
+    @Operation(summary = "Update pizza.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK."),
+        @ApiResponse(responseCode = "400", description = "Bad Request."),
+        @ApiResponse(responseCode = "404", description = "Not found."),
+        @ApiResponse(responseCode = "422", description = "Unprocessable Entity.")
+    })
+    @PutMapping("/{pizza_id}")
+    public void updatePizza(@PathVariable Long pizzaId, @RequestBody PizzaDTO pizzaDTO, HttpServletResponse response) {
         pizzaService.updatePizza(pizzaDTO);
         response.setStatus(HttpStatus.NO_CONTENT.value());
     }
 
+    @Operation(summary = "Delete pizza.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK."),
+        @ApiResponse(responseCode = "404", description = "Not found.")
+    })
     @DeleteMapping("/{pizza_id}")
     public void deletePizza(@PathVariable Long pizzaId, HttpServletResponse response) {
         pizzaService.deletePizza(pizzaId);
